53767648924f2b6bd93858c583bc6bf4fff1f951
Mario Santos <mariocsants@gmail.com>
2019-01-03 11:06:26 -0200

    Configuração da home

---
+++ lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Text('Troca almoço'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
